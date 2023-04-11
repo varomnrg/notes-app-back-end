e9e260e92a05ffdcea959e300d480390c0179497
varomnrg <varomnrg@gmail.com>
2023-04-11 22:15:48 +0700

    added authentications

--- src/server.js
+++ src/server.js
@@ -1,48 +1,90 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Hapi = require('@hapi/hapi');
+const Hapi = require("@hapi/hapi");
+const Jwt = require("@hapi/jwt");
 
-const notes =  require('./api/notes')
-const NotesService = require('./services/postgres/NotesService')
-const NotesValidator = require('./validator/notes')
+//notes
+const notes = require("./api/notes");
+const NotesService = require("./services/postgres/NotesService");
+const NotesValidator = require("./validator/notes");
 
-const users = require('./api/users');
-const UsersService = require('./services/postgres/UsersService');
-const UsersValidator = require('./validator/users');
+//users
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
 
-const init = async () => {  
-  const notesService = new NotesService();
-  const usersService = new UsersService(); 
+// authentications
+const authentications = require("./api/authentications");
+const AuthenticationsService = require("./services/postgres/AuthenticationsService");
+const TokenManager = require("./tokenize/TokenManager");
+const AuthenticationsValidator = require("./validator/authentications");
 
-  const server = Hapi.server({
-    port: process.env.PORT,
-    host: process.env.HOST,
-    routes: {
-      cors: {
-        origin: ['*'],
-      },
-    },
-  });
+const init = async () => {
+    const notesService = new NotesService();
+    const usersService = new UsersService();
+    const authenticationsService = new AuthenticationsService();
 
-  await server.register([
-    {
-      plugin: notes,
-      options: {
-        service: notesService,
-        validator: NotesValidator,
-      },
-    },
-    {
-      plugin: users,
-      options: {
-        service: usersService,
-        validator:  UsersValidator,
-      }
-    }
-  ]);
+    const server = Hapi.server({
+        port: process.env.PORT,
+        host: process.env.HOST,
+        routes: {
+            cors: {
+                origin: ["*"],
+            },
+        },
+    });
 
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+    // registrasi plugin eksternal
+    await server.register([
+        {
+            plugin: Jwt,
+        },
+    ]);
+
+    server.auth.strategy("notesapp_jwt", "jwt", {
+        keys: process.env.ACCESS_TOKEN_KEY,
+        verify: {
+            aud: false,
+            iss: false,
+            sub: false,
+            maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+        },
+        validate: (artifacts) => ({
+            isValid: true,  
+            credentials: {
+                id: artifacts.decoded.payload.id,
+            },
+        }),
+    });
+
+    await server.register([
+        {
+            plugin: notes,
+            options: {
+                service: notesService,
+                validator: NotesValidator,
+            },
+        },
+        {
+            plugin: users,
+            options: {
+                service: usersService,
+                validator: UsersValidator,
+            },
+        },
+        {
+            plugin: authentications,
+            options: {
+                authenticationsService,
+                usersService,
+                tokenManager: TokenManager,
+                validator: AuthenticationsValidator,
+            },
+        },
+    ]);
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
