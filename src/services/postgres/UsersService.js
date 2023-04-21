59b18a286d2c8b8958c33ab08db6a8ffa512c231
varomnrg <varomnrg@gmail.com>
2023-04-21 15:45:46 +0700

    exports notes implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -41,19 +41,6 @@ class UsersService {
         }
     }
 
-    async verifyNewUsername(username) {
-        const query = {
-            text: "SELECT username FROM users WHERE username = $1",
-            values: [username],
-        };
-
-        const result = await this._pool.query(query);
-
-        if (result.rows.length > 0) {
-            throw new InvariantError("Gagal menambahkan user. Username sudah digunakan.");
-        }
-    }
-
     async getUserById(userId) {
         const query = {
             text: "SELECT id, username, fullname FROM users WHERE id = $1",
